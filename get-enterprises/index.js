ed7f5d599667cbc9015c5ef2af02282222c6c4d1
Ingar Helgesen <ingar.helgesen@t-fk.no>
2019-03-15 15:27:37 +0100

    Added enterprises interface

---
+++ get-enterprises/index.js
@@ -0,0 +1,47 @@
+const p360 = require('@alheimsins/p360')
+
+function unwrap (enterprises = {}) {
+  return enterprises.Enterprises && enterprises.Enterprises.EnterpriseResult ? enterprises.Enterprises.EnterpriseResult : []
+}
+
+async function getEnterprises (parameters) {
+  const options = {
+    baseUrl: process.env.P360_URL,
+    username: process.env.P360_USERNAME,
+    password: process.env.P360_PASSWORD
+  }
+
+  const client = p360(options)
+  const contactService = await client.ContactService()
+
+  const contactQuery = {
+    parameter: { ...parameters }
+  }
+
+  const { result: { GetEnterprisesResult } } = await contactService.GetEnterprises(contactQuery)
+  if (!GetEnterprisesResult || !GetEnterprisesResult.Successful) {
+    throw Error(GetEnterprisesResult || 'Unknown error - query failed')
+  }
+  return unwrap(GetEnterprisesResult, true)
+}
+
+module.exports = async function (context, request) {
+  if (request.body) {
+    try {
+      const data = await getEnterprises(request.body)
+      context.res = {
+        body: data
+      }
+    } catch (error) {
+      context.res = {
+        status: 400,
+        body: error.message
+      }
+    }
+  } else {
+    context.res = {
+      status: 400,
+      body: 'Please pass a request body'
+    }
+  }
+}
