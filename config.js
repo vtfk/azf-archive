86b82d3da798ffe703d59fd6657bf0fef78059bc
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-06-01 10:07:03 +0200

    Added logger

--- config.js
+++ config.js
@@ -1,5 +1,11 @@
 module.exports = {
   JWT_SECRET: process.env.JWT_SECRET || 'Noe skikkelig hemmelig',
+  PAPERTRAIL: {
+    DISABLE_LOGGING: (process.env.PAPERTRAIL_DISABLE_LOGGING && process.env.PAPERTRAIL_DISABLE_LOGGING === 'true') || false,
+    HOST: process.env.PAPERTRAIL_HOST,
+    HOSTNAME: process.env.PAPERTRAIL_HOSTNAME,
+    PORT: process.env.PAPERTRAIL_PORT
+  },
   P360: {
     baseUrl: process.env.P360_URL,
     username: process.env.P360_USERNAME,
