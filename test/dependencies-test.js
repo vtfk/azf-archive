0d79fbaf4da81294fb8380858805dba0bc26c462
Rune Moskvil Lyngås <rune.moskvil.lyngaas@vtfk.no>
2021-06-03 15:30:22 +0200

    Rewritten for jest

--- test/dependencies-test.js
+++ test/dependencies-test.js
@@ -1,34 +1,27 @@
-const test = require('ava')
 const { dependencies, devDependencies } = require('../package.json')
-const dropModules = ['husky']
-const isDropped = module => !dropModules.includes(module)
 
-test('ava works ok', t => {
-  t.true(true)
-})
-
-if (Object.keys(dependencies).filter(isDropped).length > 0) {
-  Object.keys(dependencies).filter(isDropped).forEach((dependency) => {
-    test(`${dependency} loads ok`, t => {
+if (Object.keys(dependencies).length > 0) {
+  Object.keys(dependencies).forEach((dependency) => {
+    test(`Dependency '${dependency}' loads ok`, () => {
       const module = require(dependency)
-      t.truthy(module)
+      expect(module).toBeTruthy()
     })
   })
 } else {
-  test('no dependecies to test', t => {
-    t.truthy(true)
+  test('no dependecies to test', () => {
+    expect(typeof '').toBe('string') // test is needed if no dependencies exists
   })
 }
 
-if (Object.keys(devDependencies).filter(isDropped).length > 0) {
-  Object.keys(devDependencies).filter(isDropped).forEach((dependency) => {
-    test(`${dependency} loads ok`, t => {
+if (Object.keys(devDependencies).length > 0) {
+  Object.keys(devDependencies).forEach((dependency) => {
+    test(`Dev dependency '${dependency}' loads ok`, () => {
       const module = require(dependency)
-      t.truthy(module)
+      expect(module).toBeTruthy()
     })
   })
 } else {
-  test('no devDependecies to test', t => {
-    t.truthy(true)
+  test('no devDependecies to test', () => {
+    expect(typeof '').toBe('string') // test is needed if no devDependencies exists
   })
 }
