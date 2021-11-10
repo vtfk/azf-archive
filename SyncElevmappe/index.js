d98d3fc929aa7e542f9146f6f89c30222a58915c
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2021-11-10 14:45:11 +0100

    added option for sending oldSsn in endpoint SyncElevmappe

--- SyncElevmappe/index.js
+++ SyncElevmappe/index.js
@@ -23,23 +23,34 @@ module.exports = async function (context, req) {
     return new HTTPError(400, 'Please pass a request body').toJSON()
   }
 
-  const { ssn } = req.body
-  const { birthdate, firstName, lastName } = req.body
+  const { ssn, oldSsn, birthdate, firstName, lastName, newSchools } = req.body
   if (!ssn && !(birthdate && firstName && lastName)) {
     logger('error', ['Missing required parameter "ssn" or "birthdate, firstname, lastname"'])
     return new HTTPError(400, 'Missing required parameter "ssn" or "birthdate, firstname, lastname"').toJSON()
   }
-  if (req.body.newSchools && !Array.isArray(req.body.newSchools)) {
+  if (newSchools && !Array.isArray(newSchools)) {
     return new HTTPError(400, 'Parameter "newSchools" must be array').toJSON()
   }
+  if (oldSsn && oldSsn.length !== 11) {
+    return new HTTPError(400, 'Parameter "oldSsn" must be of length 11').toJSON()
+  }
+  if (oldSsn && !ssn) {
+    return new HTTPError(400, 'Parameter "oldSsn" must be in combination with "ssn').toJSON()
+  }
 
-  const dsfSearchParameter = ssn ? { ssn } : { birthdate, firstName, lastName }
+  let dsfSearchParameter = {}
+  if (ssn) {
+    dsfSearchParameter = oldSsn ? { ssn, oldSsn } : { ssn }
+  } else {
+    dsfSearchParameter = { birthdate, firstName, lastName }
+  }
+
   try {
     const dsfData = await getDsfData(dsfSearchParameter)
     result.dsfPerson = repackDsfObject(dsfData.RESULT.HOV)
     result.privatePerson = await syncPrivatePerson(result.dsfPerson)
     result.elevmappe = await syncElevmappe(result.privatePerson)
-    if (req.body.newSchools) {
+    if (newSchools) {
       result.readPermissions = await syncReadPermissions(result.elevmappe.CaseNumber, req.body.newSchools)
     }
 
