a2814d8fd020e9868c4b5cc735e90361d58847ed
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2021-11-10 11:00:06 +0100

    Added option for syncReadPermission in SyncElevmappe-endpoint

--- SyncElevmappe/index.js
+++ SyncElevmappe/index.js
@@ -5,6 +5,7 @@ const getResponseObject = require('../lib/get-response-object')
 const syncPrivatePerson = require('../lib/archive/syncPrivatePerson')
 const syncElevmappe = require('../lib/archive/syncElevmappe')
 const HTTPError = require('../lib/http-error')
+const syncReadPermissions = require('../lib/archive/syncReadPermissions')
 
 module.exports = async function (context, req) {
   logConfig({
@@ -14,6 +15,8 @@ module.exports = async function (context, req) {
       excludeInvocationId: true
     }
   })
+
+  const result = {}
 
   if (!req.body) {
     logger('error', ['Please pass a request body'])
@@ -26,19 +29,21 @@ module.exports = async function (context, req) {
     logger('error', ['Missing required parameter "ssn" or "birthdate, firstname, lastname"'])
     return new HTTPError(400, 'Missing required parameter "ssn" or "birthdate, firstname, lastname"').toJSON()
   }
+  if (req.body.newSchools && !Array.isArray(req.body.newSchools)) {
+    return new HTTPError(400, 'Parameter "newSchools" must be array').toJSON()
+  }
+
   const dsfSearchParameter = ssn ? { ssn } : { birthdate, firstName, lastName }
   try {
     const dsfData = await getDsfData(dsfSearchParameter)
-    const dsfPerson = repackDsfObject(dsfData.RESULT.HOV)
-    const privatePerson = await syncPrivatePerson(dsfPerson)
-    const elevmappe = await syncElevmappe(privatePerson)
+    result.dsfPerson = repackDsfObject(dsfData.RESULT.HOV)
+    result.privatePerson = await syncPrivatePerson(result.dsfPerson)
+    result.elevmappe = await syncElevmappe(result.privatePerson)
+    if (req.body.newSchools) {
+      result.readPermissions = await syncReadPermissions(result.elevmappe.CaseNumber, req.body.newSchools)
+    }
 
-    return getResponseObject({
-      msg: 'Succesfully synced elevmappe',
-      dsfPerson,
-      privatePerson,
-      elevmappe
-    })
+    return getResponseObject({msg: 'Succesfully synced elevmappe', ...result})
   } catch (error) {
     logger('error', [error])
     if (error instanceof HTTPError) return error.toJSON()
