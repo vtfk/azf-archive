3c726a44fd19cbcbe7b322fcccff3bbab7dd6a78
Jørgen Thorsnes <jorgen.thorsnes@gmail.com>
2021-12-10 12:10:51 +0100

    Address is handled by azf-dsf

--- test/repackDsfObject.test.js
+++ test/repackDsfObject.test.js
@@ -5,33 +5,29 @@ const person = {
   PERS: '98053',
   INR: '25815100000',
   STAT: 'BOSATT',
-  'NAVN-S': 'TUNNEL',
-  'NAVN-F': 'OFFISIELL',
-  'NAVN-M': 'BANE',
-  NAVN: 'TUNNEL OFFISIELL BANE',
-  ADR: 'HUSØYVEIEN 28',
+  'NAVN-S': 'Tunnel',
+  'NAVN-F': 'Offisiell',
+  'NAVN-M': 'Bane',
+  NAVN: 'Tunnel Offisiell Bane',
+  ADR: 'Husøyveien 28',
   POSTN: '3132',
   POSTS: 'HUSØYSUND',
+  ADR1: 'Husøygata',
+  ADR2: '29',
+  ADR3: '3133 HUSØYVIK',
   'SPES-KD': '0',
-  SPES: 'VANLIG BOSATT'
-}
-
-const personOn4 = {
-  ...person,
-  'SPES-KD': '4',
-  SPES: 'KLIENTADRESSE'
-}
-
-const personOn6 = {
-  ...person,
-  'SPES-KD': '6',
-  SPES: 'SPERRET ADRESSE, STRENGT FORTROLIG'
-}
-
-const personOn7 = {
-  ...person,
-  'SPES-KD': '7',
-  SPES: 'SPERRET ADRESSE, FORTROLIG'
+  SPES: 'VANLIG BOSATT',
+  FNR: '25815198053',
+  postAdresse: {
+    ADR: 'Husøygata 29',
+    POSTN: '3133',
+    POSTS: 'HUSØYVIK'
+  },
+  bostedsAdresse: {
+    ADR: 'Husøyveien 28',
+    POSTN: '3132',
+    POSTS: 'HUSØYSUND'
+  }
 }
 
 const expectedProperties = [
@@ -73,36 +69,4 @@ test('Repack gives back an object with correct properties and layout', () => {
   const repacked = repack(person)
   expect(typeof repacked).toBe('object')
   expectedProperties.forEach(({ name, type }) => expect(typeof repacked[name]).toBe(type))
-})
-
-test('SPES-KD 0 returns object with real streetAddress', () => {
-  const repacked = repack(person)
-  expect(repacked.streetAddress.toLowerCase()).toBe(person.ADR.toLowerCase())
-  expect(repacked.zipCode).toBe(person.POSTN)
-  expect(repacked.zipPlace.toLowerCase()).toBe(person.POSTS.toLowerCase())
-  expect(repacked.addressType).toBe('normal')
-})
-
-test('SPES-KD 4 returns object without real streetAddress', () => {
-  const repacked = repack(personOn4)
-  expect(repacked.streetAddress).toBe('Klientadresse')
-  expect(repacked.zipCode).toBe('_9999')
-  expect(repacked.zipPlace).toBe('UKJENT')
-  expect(repacked.addressType).toBe('klientadresse')
-})
-
-test('SPES-KD 6 returns object without real streetAddress', () => {
-  const repacked = repack(personOn6)
-  expect(repacked.streetAddress.toLowerCase()).toBe(personOn6.SPES.toLowerCase())
-  expect(repacked.zipCode).toBe('_9999')
-  expect(repacked.zipPlace).toBe('UKJENT')
-  expect(repacked.addressType).toBe('Sperret adresse, strengt fortrolig')
-})
-
-test('SPES-KD 7 returns object without real streetAddress', () => {
-  const repacked = repack(personOn7)
-  expect(repacked.streetAddress.toLowerCase()).toBe(personOn7.SPES.toLowerCase())
-  expect(repacked.zipCode).toBe('_9999')
-  expect(repacked.zipPlace).toBe('UKJENT')
-  expect(repacked.addressType).toBe('Sperret adresse, fortrolig')
 })
