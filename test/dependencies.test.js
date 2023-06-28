8aff88293473b81bae122e7bcf105c49bf198fdf
jorgtho <jorgen.thorsnes@gmail.com>
2023-06-28 19:03:19 +0200

    dependency updates

--- test/dependencies.test.js
+++ test/dependencies.test.js
@@ -1,4 +1,4 @@
-const { dependencies, devDependencies } = require('../package.json')
+const { dependencies } = require('../package.json')
 
 if (Object.keys(dependencies).length > 0) {
   Object.keys(dependencies).forEach((dependency) => {
@@ -10,18 +10,5 @@ if (Object.keys(dependencies).length > 0) {
 } else {
   test('no dependecies to test', () => {
     expect(typeof '').toBe('string') // test is needed if no dependencies exists
-  })
-}
-
-if (Object.keys(devDependencies).length > 0) {
-  Object.keys(devDependencies).forEach((dependency) => {
-    test(`Dev dependency '${dependency}' loads ok`, () => {
-      const module = require(dependency)
-      expect(module).toBeTruthy()
-    })
-  })
-} else {
-  test('no devDependecies to test', () => {
-    expect(typeof '').toBe('string') // test is needed if no devDependencies exists
   })
 }
